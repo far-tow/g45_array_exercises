1328915b92e2ef9f1914cd2c1a50931e83c1a4d0
Farhad Towfighian <fartow@gmail.com>
2022-10-21 17:15:57 +0200

    Changed the solution by using java.utils.Arrays.

--- src/main/java/se/lexicon/Exercise04.java
+++ src/main/java/se/lexicon/Exercise04.java
@@ -1,5 +1,7 @@
 package se.lexicon;
 
+import java.util.Arrays;
+
 public class Exercise04 {
 
     /**
@@ -9,7 +11,16 @@ public class Exercise04 {
      * Elements from second array: 1 15 20
      */
     public static void ex4() {
-        int[] arryOne = {1, 15, 20};
+        //This is easier way to solve this task:
+        int[] arrays1 = {1, 15, 20};
+        //int [] arrays2 = new int[3];
+        System.out.println("Array No. 1: " + Arrays.toString(arrays1));
+        int[] arrays2 = Arrays.copyOf(arrays1, arrays1.length);
+        System.out.println("Array No. 2: " + Arrays.toString(arrays2));
+
+
+        //This is my solution to this task.
+        /*int[] arryOne = {1, 15, 20};
         int[] arrayTwo = new int[arryOne.length];
 
         for (int i = 0; i < arryOne.length; i++) {
@@ -20,7 +31,7 @@ public class Exercise04 {
         System.out.println("copy of arrayOne: ");
         for (int i = 0; i < arrayTwo.length; i++) {
             System.out.println(arrayTwo[i]);
-        }
+        }*/
 
 
     }
