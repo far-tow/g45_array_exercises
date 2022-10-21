94b9e9823af3b1aecfe084dd255e530fac9cdf1f
Farhad Towfighian <fartow@gmail.com>
2022-10-21 20:04:23 +0200

    Just different solutions but same result here!

--- src/main/java/se/lexicon/Exercise05.java
+++ src/main/java/se/lexicon/Exercise05.java
@@ -9,6 +9,25 @@ public class Exercise05 {
    *              Sweden Stockholm
    */
   public static void ex5() {
+    //This is how teacher has solve it:
+    String[][] countries = new String[2][2];
+    String[] arr = {"France", "Paris", "Sweden", "Stockholm"};
+    int index = 0;
+
+    for (int i = 0; i < countries.length; i++) {
+      for (int j = 0; j < countries.length; j++) {
+        countries[i][j] = arr[index];
+        index++;
+
+
+      }
+
+    }
+    System.out.println(countries[0][0] + ", " + countries[0][1]);
+    System.out.println(countries[1][0] + ", " + countries[1][1]);
+
+    //This is my solution:
+    /*
     String [][] places = new String[2][2];
     places [0][0] = "France";
     places [0][1] = "Paris";
@@ -17,11 +36,11 @@ public class Exercise05 {
 
     for (int i = 0; i < places.length ; i++){
       for (int j = 0 ; j < places.length ; j++){
-        System.out.print(places[i][j] + " ") ;
+        System.out.print(places[i][j] + ", ") ;
       }
       System.out.println();
 
-    }
+    }*/
 
 
 
